12b377e4aa3d1756402241f7efd76e0738a7078b
Shiyu Wang <shiyu3169@gmail.com>
2019-08-01 13:07:04 -0400

    add and remove filters

--- main.js
+++ main.js
@@ -8,10 +8,86 @@ const downloadBtn = document.querySelector("#download-btn");
 const uploadFile = document.querySelector("#upload-file");
 const revertBtn = document.querySelector("#revert-btn");
 
-// TODO: Filters
+// Add Filters & Effects
+document.addEventListener("click", e => {
+  if (e.target.classList.contains("filter-btn")) {
+    if (e.target.classList.contains("brightness-add")) {
+      Caman("#canvas", img, function() {
+        this.brightness(5).render();
+      });
+    } else if (e.target.classList.contains("brightness-remove")) {
+      Caman("#canvas", img, function() {
+        this.brightness(-5).render();
+      });
+    } else if (e.target.classList.contains("contrast-add")) {
+      Caman("#canvas", img, function() {
+        this.contrast(5).render();
+      });
+    } else if (e.target.classList.contains("contrast-remove")) {
+      Caman("#canvas", img, function() {
+        this.contrast(-5).render();
+      });
+    } else if (e.target.classList.contains("saturation-add")) {
+      Caman("#canvas", img, function() {
+        this.saturation(5).render();
+      });
+    } else if (e.target.classList.contains("saturation-remove")) {
+      Caman("#canvas", img, function() {
+        this.saturation(-5).render();
+      });
+    } else if (e.target.classList.contains("vibrance-add")) {
+      Caman("#canvas", img, function() {
+        this.vibrance(5).render();
+      });
+    } else if (e.target.classList.contains("vibrance-remove")) {
+      Caman("#canvas", img, function() {
+        this.vibrance(-5).render();
+      });
+    } else if (e.target.classList.contains("vintage-add")) {
+      Caman("#canvas", img, function() {
+        this.vintage().render();
+      });
+    } else if (e.target.classList.contains("lomo-add")) {
+      Caman("#canvas", img, function() {
+        this.lomo().render();
+      });
+    } else if (e.target.classList.contains("clarity-add")) {
+      Caman("#canvas", img, function() {
+        this.clarity().render();
+      });
+    } else if (e.target.classList.contains("sincity-add")) {
+      Caman("#canvas", img, function() {
+        this.sinCity().render();
+      });
+    } else if (e.target.classList.contains("crossprocess-add")) {
+      Caman("#canvas", img, function() {
+        this.crossProcess().render();
+      });
+    } else if (e.target.classList.contains("pinhole-add")) {
+      Caman("#canvas", img, function() {
+        this.pinhole().render();
+      });
+    } else if (e.target.classList.contains("nostalgia-add")) {
+      Caman("#canvas", img, function() {
+        this.nostalgia().render();
+      });
+    } else if (e.target.classList.contains("hermajesty-add")) {
+      Caman("#canvas", img, function() {
+        this.herMajesty().render();
+      });
+    }
+  }
+});
+
+// Revert Filters
+revertBtn.addEventListener("click", e => {
+  Caman("#canvas", img, function() {
+    this.revert();
+  });
+});
 
 // Upload File
-uploadFile.addEventListener("change", e => {
+uploadFile.addEventListener("change", () => {
   // Get File
   const file = document.querySelector("#upload-file").files[0];
 
